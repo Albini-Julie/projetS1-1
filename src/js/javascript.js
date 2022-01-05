41f735b7de2ea5a907ad9badf59871327cdbea81
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2022-01-05 15:30:12 +0100

    mise en place du carrousel

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -37,23 +37,22 @@ function carrousel() {
 
     anim15.add({
         targets: article__carrousel,
-        translateX: [{value: '500', duration: d},
-                     {value: '-500', duration: 0, delay: function(img,ind) {
-                         if (ind == 0) return (11*delta)+(10*d);
-                         if (ind == 1) return (10*delta)+(9*d);
-                         if (ind == 2) return (9*delta)+(8*d);
-                         if (ind == 3) return(8*delta)+(7*d);
-                         if (ind == 4) return (7*delta)+(6*d);
-                         if (ind == 5) return (6*delta)+(5*d);
-                         if (ind == 6) return (5*delta)+(4*d);
-                         if (ind == 7) return (4*delta)+(3*d);
-                         if (ind == 8) return (3*delta)+(2*d);
-                         if ind == 9) return (2*delta)+d;
-                         if ind == 10) return delta;
+        translateX: [{value: '280', duration: d},
+                     {value: '-280', duration: 0, delay: function(img,ind) {
+                         if (ind == 0) return (10*delta)+(9*d);
+                         if (ind == 1) return (9*delta)+(8*d);
+                         if (ind == 2) return(8*delta)+(7*d);
+                         if (ind == 3) return (7*delta)+(6*d);
+                         if (ind == 4) return (6*delta)+(5*d);
+                         if (ind == 5) return (5*delta)+(4*d);
+                         if (ind == 6) return (4*delta)+(3*d);
+                         if (ind == 7) return (3*delta)+(2*d);
+                         if (ind == 8) return (2*delta)+d;
+                         if (ind == 9) return delta;
                          return 0;
                      }},
                      {value: '0', duration: function(img,ind) {
-                        if (ind == 3) return 0;
+                        if (ind == 10) return 0;
                         return d;
                     }}],
         easing: 'linear',
@@ -68,8 +67,8 @@ function carrousel() {
             if (ind == 7) return (8*delta)+(7*d);
             if (ind == 8) return (9*delta)+(8*d);
             if (ind == 9) return (10*delta)+(9*d);
-            if (ind == 10) return (11)
-            return (4*delta)+(3*d);
+            if (ind == 10) return (11*delta)+(10*d);
+            return (12*delta)+(11*d);
         }
     });
 }
